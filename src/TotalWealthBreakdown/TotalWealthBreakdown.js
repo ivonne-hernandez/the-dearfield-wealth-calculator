90ead1159a616028c989f85684ec07ce039e48fd
Ivonne Hernandez <ivonne702@gmail.com>
2022-03-07 19:09:33 -0700

    Refactored classNames to TotalWealthBreakdown for testing purposes

--- src/TotalWealthBreakdown/TotalWealthBreakdown.js
+++ src/TotalWealthBreakdown/TotalWealthBreakdown.js
@@ -13,29 +13,29 @@ const TotalWealthBreakdown = ({ homePrice, downPaymentContribution, dearfieldFun
       <h4 className='total-wealth-output-title'>
         Total Down Payment Brought to Closing: 
       </h4>
-      <p className='total-wealth-output'>
+      <p className='total-wealth-output-down-payment'>
         ${totalDownPaymentOnClosing.toFixed(2)}
       </p>
       <h4 className='total-wealth-output-title'>
         Estimated Home Appreciation in 10 years: 
       </h4>
-      <p className='total-wealth-output'>
+      <p className='total-wealth-output-appreciation'>
         ${estimatedHomeAppreciationAfterTenYears.toFixed(2)}
       </p>
       <h4>Down Payment Assistance Repaid to The Dearfield Fund:</h4>
-      <p className='total-wealth-output'>
+      <p className='total-wealth-output-repaid'>
         ${dearfieldFundContribution ? dearfieldFundContribution.toFixed(2) : '0.00'}
       </p>
       <h4>Five of My Home's 10-Year Appreciation Paid to The Dearfield Fund:</h4>
-      <p className='total-wealth-output'>
+      <p className='total-wealth-output-five-percent'>
         ${fivePercentOfHomeAppreciation.toFixed(2)}
       </p>
       <h4>Wealth Reinvested to Help Future Dearfield Homeowners:</h4>
-      <p className='total-wealth-output'>
+      <p className='total-wealth-output-reinvested'>
         ${totalRepaymentToDearfieldFund.toFixed(2)}
       </p>
       <h4>My Total Wealth:</h4>
-      <p className='total-wealth-output'>
+      <p className='total-wealth-output-total'>
         ${myTotalWealth.toFixed(2)}
       </p>
     </div>
